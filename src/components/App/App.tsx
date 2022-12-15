fa147d349e9faf0ae924d7e24a0eec9e20e2e538
alekseikorotkov <korotkovaleksei@icloud.com>
2022-12-15 15:37:45 +0200

    Init Project

---
+++ src/components/App/App.tsx
@@ -0,0 +1,16 @@
+import PlayerCard from "../PlayerCard/PlayerCard";
+const App = (): JSX.Element => {
+  return (
+    <div>
+        <a
+          href="https://www.figma.com/file/i9nWRlDrDbqruPpyaXhy8W/player-card?node-id=2%3A5665"
+          target="_blank" rel="noreferrer"
+        >
+          <h1>MUI II. Layout and theme</h1>
+        </a>
+        <PlayerCard />
+      </div>
+  );
+}
+
+export default App;
